a13b7aaf6ba3f9a8d1642173c0f891a62b738c3c
zhenghao8 <35733641+zhenghao8@users.noreply.github.com>
2025-07-20 16:21:22 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.ibanzou.xyz"];
+pub const RS_PUB_KEY: &str = "suyNY3cbiQg1uWmvg8k2yCrPzTmoKFUAU5oip5KK7Hc=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
